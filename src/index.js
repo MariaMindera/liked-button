6447563def6e8f652a206cdc0f424528c76b199c
mariapereira <mariapereira5a20@gmail.com>
2020-03-23 12:00:31 +0000

    Solved

--- src/index.js
+++ src/index.js
@@ -1,38 +1,50 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import ReactDOM from "react-dom";
 import cx from "classnames";
 
 import "./styles.css";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { count: 100 };
-    this.liked = false;
-  }
-
-  like = () => {
-    if (!this.liked) {
-      this.liked = true;
-      this.setState(({ count }) => ({ count: count + 1 }));
-      return;
+    constructor(props) {
+        super(props);
+        this.state = { count: 100, error: "" };
+        this.liked = false;
     }
-    this.liked = false;
-    this.setState(({ count }) => ({ count: count - 1 }));
-  };
 
-  render() {
-    const { count } = this.state;
-    return (
-      <button
-        type="button"
-        onClick={this.like}
-        className={cx("like-button", { liked: this.liked })}
-      >
-        Like | <span className="likes-counter">{count}</span>
-      </button>
-    );
-  }
+    like = () => {
+        fetch("http://localhost:3000/")
+            .then(data => data.json())
+            .then(data => {
+                if (data) {
+                    this.liked = !this.liked;
+                    this.setState(({ count }) =>
+                        this.liked
+                            ? { count: count + 1, error: "" }
+                            : { count: count + 1, error: "" }
+                    );
+                } else {
+                    this.setState(() => ({
+                        error: "ERROR"
+                    }));
+                }
+            });
+    };
+
+    render() {
+        const { count, error } = this.state;
+        return (
+            <Fragment>
+                <button
+                    type="button"
+                    onClick={this.like}
+                    className={cx("like-button", { liked: this.liked })}
+                >
+                    Like | <span className="likes-counter">{count}</span>
+                </button>
+                <p>{error}</p>
+            </Fragment>
+        );
+    }
 }
 
 const rootElement = document.getElementById("root");
